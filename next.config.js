3b679767345a9b98830731183bc835fe3ceb99fa
Admin <support@unlimitpotential.com>
2023-01-26 03:13:17 -0500

    s

--- next.config.js
+++ next.config.js
@@ -1,3 +1,26 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+
+// @ts-ignore
 module.exports = {
-  reactStrictMode: true
-};
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'abs.twimg.com',
+      'pbs.twimg.com',
+      'api.producthunt.com',
+      'avatars.githubusercontent.com',
+      'images.clerk.dev',
+      'cdn.builder.io',
+      'imagedelivery.net',
+      'framerusercontent.com',
+      'k4j3j2s7.rocketcdn.me',
+      'genki.fueko.net',
+      'okiro-dark.fueko.net',
+      'assets-global.website-files.com'
+    ],
+  },
+}
